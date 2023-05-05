06813dcfb5a1451b8b74d45d32744c217f80b944
Eleonora Prokushenkova <eleonoraprokushenkova@MacBook-Air-Eleonora.local>
2023-05-05 14:48:50 +0300

    add AntD card

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,19 @@
 export const getAllProducts = () => {
-  return fetch("https://dummyjson.com/products")
-    .then((res) => res.json());
+  return fetch("https://dummyjson.com/products").then((res) => res.json());
+};
+
+export const addToCart = (id) => {
+  return fetch("https://dummyjson.com/carts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: 1,
+      products: [
+        {
+          id,
+          quantity: 1
+        }
+      ]
+    })
+  }).then((res) => res.json());
 };
--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,57 +1,103 @@
 import { useEffect, useState } from "react";
-import { getAllProducts } from "../../API";
-import { Card, List, Image, Typography } from "antd";
+import { addToCart, getAllProducts } from "../../API";
+import {
+  Card,
+  List,
+  Image,
+  Typography,
+  Badge,
+  Rate,
+  Button,
+  message
+} from "antd";
+import PropTypes from "prop-types";
 
 const Products = () => {
   const [items, setItems] = useState([]);
-  // айтем это то что у нас приходит выдуманное название ,можно было написать и продакс это все равно что продакс сет продактс
 
   useEffect(() => {
     getAllProducts().then((res) => {
       setItems(res.products);
     });
   }, []);
+
+  function AddToCartButton({ item }) {
+    const addProductToCart = () => {
+      addToCart(item.id).then((res) => {
+        message.success(`${item.title} has been added to cart!`);
+      });
+    };
+    return (
+      <Button
+        type="link"
+        onClick={() => {
+          addProductToCart();
+        }}
+      >
+        Add to Cart
+      </Button>
+    );
+  }
   return (
     <div>
       <List
         grid={{ column: 3 }}
         renderItem={(product, index) => {
           return (
-            <Card
-              title={product.title}
-              key={index}
-              cover={
-                <Image className="itemCardImage" src={product.thumbnail} />
-              } // картинки сами
+            <Badge.Ribbon
+              className="itemCardBadge"
+              text={product.discountPercentage}
+              color="cyan"
             >
-              <Card.Meta
-                title={
-                  <Typography.Paragraph>
-                    Price: {product.price} ${" "}
-                    <Typography.Text>
+              <Card
+                className="itemCard"
+                title={product.title}
+                key={index}
+                cover={
+                  <Image className="itemCardImage" src={product.thumbnail} />
+                }
+                actions={[
+                  <div key="actions">
+                    <Rate allowHalf disabled value={product.rating} />
+                    <AddToCartButton item={product} />
+                  </div>
+                ]}
+              >
+                <Card.Meta
+                  title={
+                    <Typography.Paragraph>
+                      Price: {product.price} ${" "}
+                      <Typography.Text>
+                        {" "}
+                        $
+                        {parseFloat(
+                          product.price +
+                            (product.price * product.discountPercentage) / 100
+                        ).toFixed(2)}
+                      </Typography.Text>
+                    </Typography.Paragraph>
+                  }
+                  description={
+                    <Typography.Paragraph
+                      ellipsis={{ rows: 2, expandable: true, symbol: "more" }}
+                    >
                       {" "}
-                      $
-                      {parseFloat(
-                        product.price +
-                          (product.price * product.discountPercentage) / 100
-                      ).toFixed(2)}
-                    </Typography.Text>
-                  </Typography.Paragraph>
-                }
-                description={
-                  <Typography.Paragraph>
-                    {" "}
-                    {product.description}{" "}
-                  </Typography.Paragraph>
-                }
-              ></Card.Meta>
-            </Card>
+                      {product.description}{" "}
+                    </Typography.Paragraph>
+                  }
+                ></Card.Meta>
+              </Card>
+            </Badge.Ribbon>
           );
         }}
         dataSource={items}
       ></List>
     </div>
   );
+};
+
+Products.propTypes = {
+  item: PropTypes.object
 };
 
 export default Products;
